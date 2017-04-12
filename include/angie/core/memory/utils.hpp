56539c598bba058bff25fc03c937a17e7d8345c0
fabiopolimeni <fabio.polimeni@outlook.com>
2017-04-12 09:03:07 +0200

    Added submodule externals/dbgbtools

---
+++ include/angie/core/memory/utils.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2017 Fabio Polimeni
+// Created on: 11/04/2017
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#pragma once
---
+++ test/angie/core/containers/array_tests.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2017 Fabio Polimeni
+// Created on: 09/04/2017
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
